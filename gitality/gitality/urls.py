f614b11105b264658fb108edcc783d5c33823656
Dima Moroz <me@dimamoroz.com>
2013-09-28 15:21:46 +0300

    Social auth using github implemented

--- gitality/gitality/urls.py
+++ gitality/gitality/urls.py
@@ -8,7 +8,14 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # url(r'^$', 'gitality.views.home', name='home'),
     # url(r'^gitality/', include('gitality.foo.urls')),
+
     url(r'^$', 'core.views.home', name='home'),
+
+    url(r'^login/$', 'core.views.login', name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+        {'next_page': settings.LOGIN_REDIRECT_URL}, name='logout'),
+    url(r'^error/$', 'core.views.error', name='error'),
+
     url(r'^auth/', include('social_auth.urls')),
     url(r'^projects/', include('projects.urls', namespace='projects')),
     url(r'^admin/', include(admin.site.urls)),
