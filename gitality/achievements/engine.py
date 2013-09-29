ed45a491c4484490d49db8e8280efbb1bbb70aa2
Dima Moroz <me@dimamoroz.com>
2013-09-29 16:40:16 +0300

    AChievement engine and celery task for achievement inspection

---
+++ gitality/achievements/engine.py
@@ -0,0 +1,17 @@
+from .tasks import inspect_achievement
+
+
+class AchievementsEngine(object):
+    """
+    Handles progress state changes,
+    inspects and unlocks achievements.
+    """
+
+    def __init__(self, achievements):
+        self.achievements = achievements
+
+    def handle(self, **kwargs):
+        """
+        Serves as a signal receiver handler.
+        """
+        inspect_achievement.delay(self.achievements, **kwargs)
