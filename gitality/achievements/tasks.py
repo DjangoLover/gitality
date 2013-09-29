79431b2ce5d3b834bfee2683fc94b5616dd8b6ab
Dima Moroz <me@dimamoroz.com>
2013-09-29 19:37:02 +0300

    Added distinct to achievements filter

--- gitality/achievements/tasks.py
+++ gitality/achievements/tasks.py
@@ -6,12 +6,15 @@ def inspect_achievement(achievements, entity_type, entity, dirty_fields):
     """
     Background celery task for achievements inspection.
     """
+
     # Getting only achievements that have sense to be checked
     achievements = achievements.filter(
         entity_type=entity_type,
         requirements__key__in=dirty_fields.keys()
     # NOTE: Excluding unlocked achievements
-    ).exclude(id__in=entity.achievements.values_list('achievement_id', flat=True))
+    ).exclude(
+        id__in=entity.achievements.values_list('achievement_id', flat=True)
+    ).distinct()
 
     # Inspecting
     for achievement in achievements:
