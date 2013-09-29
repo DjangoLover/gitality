ed948b6d37922df92559426cc4e4eb06b415d04c
postatum <postatum@gmail.com>
2013-09-29 21:14:41 +0300

    Styling

--- gitality/core/views.py
+++ gitality/core/views.py
@@ -9,11 +9,11 @@ from achievements.models import (
 
 def home(request):
     latest_comitauthor = CommitAuthorAchievement.objects.order_by('created')
-    latest_comit = CommitAchievement.objects.order_by('created')
+    # latest_comit = CommitAchievement.objects.order_by('created')
     latest_proj = ProjectAchievement.objects.order_by('created')
     context = {
         'latest_comitauthor': latest_comitauthor[:20],
-        'latest_comit': latest_comit[:20],
+        # 'latest_comit': latest_comit[:20],
         'latest_proj': latest_proj[:20],
     }
     return render(request, 'core/index.html', context)
